10cf5597efb7615730745d249e7c02e7559f999e
Ashu Singh <Ashusingh2201@gmail.com>
2026-05-26 21:50:09 +0530

    load variabled for the mail service nodemailer and make a function which gets the email and token to send a

---
+++ backend/src/services/mail.service.ts
@@ -0,0 +1,17 @@
+import { transporter } from "../constant/mail.js";
+import env from '../constant/env.js'
+
+export const sendVerificationEmail=async(email:string,token:string)=>{
+
+  const verificationLink=`${env.CLIENT_URL}/verify-email?token=${token}`
+
+  await transporter.sendMail({
+    from :`"FinSer" <${env.MAIL_USER}>`,
+    to:email,
+    subject:"Verify your email",
+    html:`<h2>Verify your email</h2>
+          <p>Click on the link below to verify your email account:</p>
+          <a href="${verificationLink}">Verify email</a>
+          <p>This link will expire in 15 minutes.</p>`
+  })
+}
