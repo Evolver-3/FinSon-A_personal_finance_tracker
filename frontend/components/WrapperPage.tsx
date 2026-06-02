5a04c304b9ca3ad73b74e5bf13bcfd141db1a6d5
Ashu Singh <Ashusingh2201@gmail.com>
2026-06-02 23:42:13 +0530

    wrapper created for every page of app

---
+++ frontend/components/WrapperPage.tsx
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native'
+import React, { ReactNode } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+type wrapperProps={
+  children:ReactNode
+}
+const Wrapper = ({children}:wrapperProps) => {
+  return (
+<SafeAreaView className="flex-1 bg-slate-50 dark:bg-neutral-950 ">
+    <View className='flex-1 bg-neutral-950 dark:bg-rose-100'>
+      {children}
+    </View>
+  </SafeAreaView>
+)}
+
+export default Wrapper
