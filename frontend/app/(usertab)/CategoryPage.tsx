d954e4f5f803f7144ade1662d6d96adf8ee663e8
Ashu Singh <Ashusingh2201@gmail.com>
2026-06-11 23:36:16 +0530

    a category page is defined which will show all categories and adding a new category

--- frontend/app/(usertab)/CategoryPage.tsx
+++ frontend/app/(usertab)/CategoryPage.tsx
@@ -1,13 +1,14 @@
-import { View, Text ,Pressable, Modal} from 'react-native'
+import { View, Text ,Pressable, Modal, FlatList} from 'react-native'
 import React,{useState} from 'react'
 import Wrapper from '@/components/WrapperPage'
 import TopUserComp from '@/components/comps/TopUserComp'
-import { Cross, Plus, Search } from 'lucide-react-native'
+import { Cross, Pencil, Plus, Search } from 'lucide-react-native'
 import { useCategory } from '@/hooks/useCategory'
 import { ButtonNeed, TextData } from '../(auth)/sign-in'
+import RenderCategory from '@/components/category/RenderCategory'
 
 const CategoryPage = () => {
-  const {fetchAllCategory,fetchCategory,categories,loading}=useCategory()
+  const {fetchAllCategory,fetchCategory,categories,loading,creatingCategory}=useCategory()
   const [visible,setVisible]=useState(false)
 
   const [name,setName]=useState("")
@@ -16,6 +17,11 @@ const CategoryPage = () => {
   const [type,setType]=useState<"INCOME" | "EXPENSE">("EXPENSE")
 
   const handleSubmit=async()=>{
+    await creatingCategory({name,type,color})
+
+    setName("")
+    setColor("#6366f1")
+    setVisible(false)
 
   }
   console.log(categories)
@@ -24,7 +30,7 @@ const CategoryPage = () => {
       <View className='flex-1 px-4 gap-y-4 pt-4'>
         <TopUserComp
         headingText={"Category"}/>
-
+ 
         <View className='flex-row items-center gap-x-2'>
 
           <View className='flex-row border border-neutral-500 rounded-lg w-8/10 p-2 gap-x-4 flex-grow'>
@@ -33,7 +39,7 @@ const CategoryPage = () => {
 
           </View>
 
-          <View className='rounded-lg  p-2 bg-neutral-600 border border-neutral-600'>
+          <View className='rounded-lg p-2 bg-neutral-600 border border-neutral-600'>
             <Pressable
             onPress={()=>setVisible(true)}>
               <Plus
@@ -51,6 +57,8 @@ const CategoryPage = () => {
             <Pressable className='flex-1 bg-black/50 justify-center items-center'
             onPress={()=>setVisible(false)}>
 
+              <Pressable
+              onPress={(e)=>e.stopPropagation()}>
               <View className='bg-neutral-900 rounded-xl p-6 w-4/5 shadow-md flex-col gap-y-4'>
 
               <Text className='text-white text-md font-extralight'>
@@ -66,6 +74,19 @@ const CategoryPage = () => {
                   onChangeText={setName}
                   secureTextEntry={false}/>
 
+                  <View className="flex-row gap-x-3">
+                    <SelectType
+                    focused={type==="INCOME"}
+                    onPress={()=>setType("INCOME")}
+                    text='Income'/>
+
+                    <SelectType
+                    focused={type==="EXPENSE"}
+                    onPress={()=>setType("EXPENSE")}
+                    text='Expense'/>
+                  </View>
+
+                 
                   <ButtonNeed
                   onPress={handleSubmit}
                   text={'Create new'}
@@ -75,25 +96,15 @@ const CategoryPage = () => {
 
               </View>
               </View>
+              </Pressable>
 
             </Pressable>
 
           </Modal>
         </View>
 
-        <View>
-          {categories.length===0? (
-            <View>
-              <Text className='text-white'>Add Categories</Text>
-            </View>
-
-          ):(
-            <View>
-            </View>
-
-          )}
-          
-        </View>
+        <RenderCategory
+        categories={categories}/>
         
 
       </View>
@@ -102,3 +113,19 @@ const CategoryPage = () => {
 }
 
 export default CategoryPage
+
+type SelectTypeProps={
+  text:string 
+  onPress:()=>void
+  focused:boolean
+
+}
+export const SelectType=({text,onPress,focused}:SelectTypeProps)=>{
+  return (
+    <Pressable
+    className={`flex-1 px-2 py-1 rounded-lg items-center ${focused ? "bg-green-500":"bg-neutral-500"}`}
+    onPress={onPress}>
+      <Text>{text}</Text>
+    </Pressable>
+  )
+}
