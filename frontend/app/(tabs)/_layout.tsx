dc5dfa3809300ab0d90006aa7d0eedf0a77485bd
Ashu Singh <Ashusingh2201@gmail.com>
2026-06-11 23:35:20 +0530

    with data.ts the code is more simpler now

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -1,25 +1,9 @@
 import { useAuthContext } from "@/context/AuthContext"
 import { Redirect, Tabs } from "expo-router"
 import { ActivityIndicator, View ,Text} from "react-native"
-import {BadgePlus, Banknote, ChartNoAxesCombinedIcon, Home, UserRound} from 'lucide-react-native'
+import { bottomTabData } from "@/data"
 
-type TabIconProps={
-  focused:boolean 
-  icon:React.ReactNode
-  text:string
-}
 
-const TabIcon=({focused,icon,text}:TabIconProps)=>{
-  return (
-    <View className={`h-10 w-10 items-center justify-center rounded-full ${focused?"#a3a3a3":"bg-transparent"}`}>
-      {icon}
-      <View>
-        <Text>{text}</Text>
-      </View>
-
-    </View>
-  )
-}
 
 export default function TabsLayout() {
   const { user,loading } = useAuthContext()
@@ -44,104 +28,60 @@ export default function TabsLayout() {
             tabBarStyle:{
               position:'absolute',
               bottom:0,
-              height:80,
-              marginHorizontal:"auto",
+              height:60,
               borderRadius:0,
               borderTopStartRadius:10,
               borderTopEndRadius:10,
               elevation:0,
               borderTopWidth:0,
-              backgroundColor:"#383534"
+              backgroundColor:"#383534",
+              gap:0
             },
             tabBarItemStyle:{
-              paddingVertical:20
+              paddingVertical:10,
+              flex:1
+
             }
           }}>
+            {bottomTabData.map((data)=>(
             <Tabs.Screen
-            name="home"
+            key={data.id}
+            name={data.name}
+         
             options={{
               tabBarIcon:({focused})=>(
                 <TabIcon
-                text={'Home'}
+                text={data.text}
                 focused={focused}
-                icon={
-                  <Home
-                  size={22}
-                  color={focused?"#0a0a0a":"#a3a3a3"}
-                  strokeWidth={focused?2.8:2}
-                />
-            }/>
-
-            
-          )
+                icon={data.icon(focused)} />
+              )
             }}/>
-
-            <Tabs.Screen
-            name="Add"
-            options={{
-              tabBarIcon:({focused})=>(
-                <TabIcon
-                text={'Add'}
-                focused={focused}
-                icon={
-                  <BadgePlus
-                  size={22}
-                  color={focused?"#0a0a0a":"#a3a3a3"}
-                  strokeWidth={focused?2.8:2}
-                />
-            }/>
-            )}}/>
-
-            <Tabs.Screen
-            name="profile"
-            options={{
-              tabBarIcon:({focused})=>(
-                <TabIcon
-                text={'Profile'}
-                focused={focused}
-                icon={
-                  <UserRound
-                  size={22}
-                  color={focused?"#0a0a0a":"#a3a3a3"}
-                  strokeWidth={focused?2.8:2}
-                />
-            }/>
-            )}}/>
-
-            <Tabs.Screen
-            name="Transactions"
-            options={{
-              tabBarIcon:({focused})=>(
-                <TabIcon
-                text={'Transactions'}
-                focused={focused}
-                icon={
-                  <Banknote
-                  size={22}
-                  color={focused?"#0a0a0a":"#a3a3a3"}
-                  strokeWidth={focused?2.8:2}
-                />
-            }/>
-            )}}/>
-
-            <Tabs.Screen
-            name="Budget"
-            options={{
-              tabBarIcon:({focused})=>(
-                <TabIcon
-                text={'Budget'}
-                focused={focused}
-                icon={
-                  <ChartNoAxesCombinedIcon
-                  size={22}
-                  color={focused?"#0a0a0a":"#a3a3a3"}
-                  strokeWidth={focused?2.8:2}
-                />
-            }/>
-            )}}/>
-
-            
-            </Tabs>
+           ))}
+            </Tabs> 
 }
 
 
+
+type TabIconProps={
+  focused:boolean 
+  icon: React.ReactNode
+  text:string
+}
+
+const TabIcon=({focused,icon,text}:TabIconProps)=>{
+  return (
+    <View className="flex-1 items-center justify-center"
+    >
+      <View>
+        {icon}
+      </View>
+        <View>
+          <Text className=' text-font-extralight leading-tight text-center text-[10px]'
+        style={{
+         color:focused ? "#0a0a0a" : "#a3a3a3"
+        }}>{text}</Text>
+        </View>
+      
+    </View>
+  )
+}
