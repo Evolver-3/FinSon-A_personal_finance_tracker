f2398a4959b3694fddc8353bef02bc9b2b39c78b
Ashu Singh <Ashusingh2201@gmail.com>
2026-06-13 22:16:42 +0530

    now category colors updated and icon category added

--- frontend/data.tsx
+++ frontend/data.tsx
@@ -1,4 +1,4 @@
-import { Home, BadgePlus, UserRound, Banknote, ChartNoAxesCombinedIcon } from 'lucide-react-native'
+import { Home, BadgePlus, UserRound, Banknote, ChartNoAxesCombinedIcon, Utensils ,Fuel, Hamburger, ShoppingCart, GlobeCheck} from 'lucide-react-native'
 import React from 'react'
 
 type bottomTabDataProps={
@@ -72,6 +72,75 @@ export const bottomTabData:bottomTabDataProps[] = [
   }
 ]
 
+export const categorydynamicColors = [
+  { btncolor: "#DB1818", colors: "#FCE8E8", darkColor:"#F39696"},
+  { btncolor: "#E67129", colors: "#FCF0F8", darkColor:"#F3BA96"},
+  { btncolor: "#E6AD29", colors: "#FCF6E8", darkColor:"#F3D796"},
+  { btncolor: "#A4E629", colors: "#F5FCE8", darkColor:"#D2F396"},
+  { btncolor:"#68E629",colors:"#EFFCE8", darkColor:"#B5F396"},
+  { btncolor:"#29E6D3",colors:"#E8FCFA", darkColor:"#96F3E9"},
+  { btncolor:"#29C0E6",colors:"#E8F8FC", darkColor:"#96E0F3"},
+  { btncolor:"#245AD1",colors:"#E8EFFC", darkColor:"#96B3F3"},
+  { btncolor:"#5529E6",colors:"#EDE8FC", darkColor:"#AC96F3"},
+  { btncolor:"#A129E6",colors:"#F5E8FC", darkColor:"#D196F3"},
+  { btncolor:"#e629ca",colors:"#fce8f9", darkColor:"#F396E5"}
+
+]
+
+type CategoryIconsProps={
+  id:number,
+  symbol:IconFn | null}
 
 
-export const categoryColors=["#E01919","#E8CF13","#3AE813","#13DDE8"] 
+export const CategoryIcons:CategoryIconsProps[] = [
+  {
+    id: 1,
+    symbol:(focused:boolean)=>(
+      <Utensils
+        size={20}
+        color={focused?"#60a5fa":"#ffffff"}
+      />
+    )},
+  {
+    id: 2,
+    symbol:(focused:boolean)=>(
+      <Fuel
+        size={20}
+        color={focused?"#60a5fa":"#ffffff"}
+      />
+
+    )
+    
+  },
+  {
+    id: 3,
+    symbol:(focused:boolean)=>(
+      
+      <Hamburger
+        size={20}
+        color={focused?"#60a5fa":"#ffffff"}
+      />
+    )
+    
+  },
+  {
+    id: 4,
+    symbol:(focused:boolean)=>( 
+      <Hamburger
+        size={20}
+        color={focused?"#60a5fa":"#ffffff"}
+      />
+    )
+    
+  },
+  {
+    id: 5,
+    symbol:(focused:boolean)=>(
+      <GlobeCheck
+        size={20}
+        color={focused?"#60a5fa":"#ffffff"}
+      />
+    )
+    
+  }
+]
