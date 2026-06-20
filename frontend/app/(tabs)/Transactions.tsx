318d3d8ea46173a7549c704e8712b2dfb1152006
Ashu Singh <Ashusingh2201@gmail.com>
2026-06-20 23:50:11 +0530

    transaction main page which will show all kinds of transacion and creating and updating with deleting too.

--- frontend/app/(tabs)/Transactions.tsx
+++ frontend/app/(tabs)/Transactions.tsx
@@ -1,4 +1,4 @@
-import { View, Text ,Pressable, Platform} from 'react-native'
+import { View, Text ,Pressable, Platform, FlatList} from 'react-native'
 import React, { useState } from 'react'
 import Wrapper from '@/components/WrapperPage'
 import { useTransaction } from '@/hooks/useTransaction'
@@ -11,6 +11,10 @@ import DateTimePicker from '@react-native-community/datetimepicker'
 import { CalendarRangeIcon } from 'lucide-react-native'
 import TopUserComp from '@/components/comps/TopUserComp'
 import SelectDropdown from '@/components/transactionPage/SelectDropdown'
+import TransactionComp from '@/components/transactionPage/TransactionComp'
+import CreateTransaction from '@/components/transactionPage/CreateTransaction'
+import HeaderList from '@/components/comps/Flat/HeaderList'
+import RenderTransaction from '@/components/transactionPage/RenderTransaction'
 
 const Transactions = () => {
   const {loading,error,transactions,creatingNewTransaction,editTransaction,removeTransaction}=useTransaction()
@@ -20,148 +24,49 @@ const Transactions = () => {
 
   const [pageLoad,setPageLoad]=useState(false)
   const [pageError,setPageError]=useState<string>('')
-
-  const [title,setTitle]=useState("")
-  const [amount,setAmount]=useState<string>('')
-  const [type,setType]=useState<"INCOME"|"EXPENSE">("EXPENSE")
-  const [note,setNote]=useState<string>('')
-  const [date,setDate]=useState<Date>(new Date())
-  const [accountId,setAccountId]=useState('')
-  const [categoryId,setCategoryId]=useState('')
-
-  const [datePickerOpen,setDatePickerOpen]=useState(false)
-
-  const handleCreateNewTransaction=async()=>{
-    try{
-      setPageLoad(true)
-      setPageError("")
-
-      await creatingNewTransaction({title,amount,type,note,date,accountId,categoryId})
-
-      setTitle('')
-      setAmount('')
-      setNote('')
-      setType("EXPENSE")
-      setDate(new Date())
-      setAccountId('')
-      setCategoryId('')
-
-
-    }catch(err:any){
-      console.log(err)
-      console.log(error)
-      const fullError=err||error
-
-      setPageError(fullError)
-    }finally{
-      setPageLoad(false)
-    }
-  }
-  console.log(transactions)
-
-
+  const [openCreate,setOpenCreate]=useState(false)
   return (
     <Wrapper loading={loading}>
-       <View className='flex-1 px-4 gap-y-4 pt-4'>
 
-        <TopUserComp
-        headingText='Budget'/>
+      <CreateTransaction
+      openCreate={openCreate}
+      setOpenCreate={setOpenCreate}
+      accounts={accounts}
+      categories={categories}
+      pageLoad={pageLoad}
+      setPageLoad={setPageLoad}
+      setPageError={setPageError}
+      creatingNewTransaction={creatingNewTransaction}
+      error={error}
 
-       <TextData
-        keyboardType="default"
-        tagexist={false}
-        tag={''}
-        value={title}
-        placeholder='Enter Title'
-        onChangeText={setTitle}
-        secureTextEntry={false}/>
-
-         <TextData
-        keyboardType="number-pad"
-        tagexist={false}
-        tag={''}
-        value={amount}
-        placeholder='Enter amount'
-        onChangeText={setAmount}
-        secureTextEntry={false}/>
-
-         <TextData
-        keyboardType="default"
-        tagexist={false}
-        tag={''}
-        value={note}
-        placeholder='Enter note'
-        onChangeText={setNote}
-        secureTextEntry={false}/>
-
-        <View className='items-center justify-center '>
-          <View className="flex-row gap-x-3 p-1 py-1 bg-neutral-500 rounded-xl">
-          <SelectType
-          focused={type==="INCOME"}
-          onPress={()=>setType("INCOME")}
-          text='Income'/>
-           
-          <SelectType
-          focused={type==="EXPENSE"}
-          onPress={()=>setType("EXPENSE")}
-          text='Expense'/>
-          </View>
+      />
+      <FlatList
+      data={transactions}
+      keyExtractor={(item)=>item.id}
+      contentContainerStyle={{paddingHorizontal:4,paddingTop:6,gap:10}}
+      
+      ListHeaderComponent={
+        <HeaderList
+        headingText={'Add new Transactions'}
+        onPress={()=>setOpenCreate(true)}
+        inlineText={"Search transactions..."}/>
+      }
+      ListEmptyComponent={
+        <View className='px-4'>
+          <Text style={{color:"#ffffff"}}>No transaction exist</Text>
         </View>
-
-        <View>
-          <Pressable
-          onPress={()=>setDatePickerOpen(true)}>
-            <CalendarRangeIcon color={"#fff"}/>
-          </Pressable>
-
-          {datePickerOpen &&(
-            <DateTimePicker
-            value={date ?? new Date()}
-            mode="date"
-            display={Platform.OS==="ios"?"spinner":"calendar"}
-            onChange={(event,pickedDate)=>{
-              if(Platform.OS!=="ios"){
-                setDatePickerOpen(false)
-              }
-              if(pickedDate){
-                setDate(pickedDate)
-              }
-            }}
-            />
-          )}
-        </View>
-       
-        <View>
-         <SelectDropdown
-         options={accounts.map(acc=>({
-          id:acc.id,
-          name:acc.name,
-          color:acc.color
-         }))}
-         selectedId={accountId}
-         onSelect={setAccountId}
-         placeholder='Select Account'/>
-
-         <SelectDropdown
-         options={categories.map(cat=>({
-          id:cat.id,
-          name:cat.name,
-          color:cat.color
-         }))}
-         selectedId={categoryId}
-         onSelect={setCategoryId}
-         placeholder='Select Category'/>
-        </View>
-
-        <ButtonNeed
-        onPress={handleCreateNewTransaction}
-        text={"Create"}
-        disabled={pageLoad}
+      }
+      renderItem={({item})=>(
+        <RenderTransaction
+        item={item}
         loading={pageLoad}
-        style={{}}
-        />
-      </View>
+        editTransaction={editTransaction}
+        removeTransaction={removeTransaction}/>
+      )}
+      />
     </Wrapper>
+
+   
   )
 }
 
