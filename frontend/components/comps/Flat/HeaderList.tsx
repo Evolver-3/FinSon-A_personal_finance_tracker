e8981a16c58e9be06c483f268e53e0534655329b
Ashu Singh <Ashusingh2201@gmail.com>
2026-06-23 19:41:45 +0530

    usage of SearchBar her

--- frontend/components/comps/Flat/HeaderList.tsx
+++ frontend/components/comps/Flat/HeaderList.tsx
@@ -1,30 +1,29 @@
-import { View, Text, Pressable} from 'react-native'
+import { View, Pressable} from 'react-native'
 import React from 'react'
 import TopUserComp from '../TopUserComp'
-import { Search, Plus} from 'lucide-react-native'
+import { Plus} from 'lucide-react-native'
 import ThemeIcon from '@/components/Theme/ThemeIcon'
+import SearchBar from '@/components/Search/SearchBar'
 
 type HeaderListProps={
   headingText:string 
   onPress:()=>void
   inlineText:string
+  setQuery:React.Dispatch<React.SetStateAction<string>>
 }
-const HeaderList = ({headingText,onPress,inlineText}:HeaderListProps) => {
+const HeaderList = ({headingText,onPress,inlineText,setQuery}:HeaderListProps) => {
+
   return (
     <View className=' px-4 gap-y-4 pt-4'>
       <TopUserComp
         headingText={headingText}/>
         
         <View className='flex-row items-center gap-x-2'>
-          <View
-            className='flex-row border border-slate-400 dark:border-neutral-500 rounded-lg w-8/10 p-2   gap-x-4 flex-grow'>
-             <ThemeIcon
-              icon={Search}
-              size={20}/>
-              <Text className='text-sm smallText font-extralight'>{inlineText}</Text>
-            </View>
+          <SearchBar
+          onSearch={setQuery}
+          inlineText={inlineText}/>
             <Pressable
-            className='rounded-lg p-2 bg-white dark:bg-neutral-600 border border-slate-200 dark:border-neutral-400'
+            className='rounded-lg p-4 bg-white dark:bg-neutral-600 border border-slate-200 dark:border-neutral-400'
             onPress={onPress}>
               <ThemeIcon
               icon={Plus}
