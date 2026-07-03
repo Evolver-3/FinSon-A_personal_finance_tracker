77c653b4384754483aac069f98bb112fd6ffef3d
Ashu Singh <Ashusingh2201@gmail.com>
2026-07-03 23:53:13 +0530

    search bar align and styling fixed

--- frontend/components/Search/SearchBar.tsx
+++ frontend/components/Search/SearchBar.tsx
@@ -2,15 +2,15 @@ import { View,TextInput} from 'react-native'
 import React,{useState} from 'react'
 import ThemeIcon from '../Theme/ThemeIcon'
 import { Search } from 'lucide-react-native'
+import { useTheme } from '@/hooks/useTheme'
 
 type SearchBarProps={
   inlineText:string
   onSearch:(query:string)=>void
 }
 
-
 const SearchBar = ({inlineText,onSearch}:SearchBarProps) => {
-
+  const {isDark}=useTheme()
   const [searchNew,setSearchNew]=useState('')
 
   const handleSearch=(text:string)=>{
@@ -19,7 +19,7 @@ const SearchBar = ({inlineText,onSearch}:SearchBarProps) => {
   }
   return (
     <View
-      className='flex-row items-center border border-slate-400 dark:border-neutral-500 rounded-lg w-8/10 p-2  gap-x-4 flex-grow'>
+      className='flex-row items-center border border-slate-400 dark:border-neutral-500 rounded-lg px-2  gap-x-4 flex-grow'>
         <ThemeIcon
         icon={Search}
         size={20}/>
@@ -30,6 +30,7 @@ const SearchBar = ({inlineText,onSearch}:SearchBarProps) => {
         onChangeText={handleSearch}
         placeholder={inlineText}
         placeholderClassName='text-sm smallText font-extralight'
+        placeholderTextColor={isDark?"#ffffff":"#000000"}
         />
     </View>
   )
