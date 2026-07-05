800c456f4fa90faca9bad768a10d42e1d88683ac
Ashu Singh <Ashusingh2201@gmail.com>
2026-07-05 23:45:19 +0530

    Budget provider is staged after authProvider, so that budgets hooks is available in whole layout

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -2,14 +2,18 @@ import { Stack } from "expo-router";
 import "../global.css"
 import { AuthProvider } from "../context/AuthContext";
 
+import { BudgetProvider } from "@/context/BudgetContext"
+
 export default function RootLayout(){
 
   return(
     <AuthProvider>
-      <Stack
-    screenOptions={{
+      <BudgetProvider>
+        <Stack
+      screenOptions={{
       headerShown:false
-    }}/>
+      }}/>
+      </BudgetProvider>
     </AuthProvider>
   )
 }
