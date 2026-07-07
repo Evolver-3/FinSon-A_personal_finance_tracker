5472506bb553dbf84a95392bc59b89b22d9da398
Ashu Singh <Ashusingh2201@gmail.com>
2026-07-07 21:55:23 +0530

    declared font hooks here, so that it is available through the app with importing

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -3,16 +3,22 @@ import "../global.css"
 import { AuthProvider } from "../context/AuthContext";
 
 import { BudgetProvider } from "@/context/BudgetContext"
+import { TransactionProvider } from "@/context/TransactionContext";
+import { useAppFonts } from "@/hooks/useAppFonts";
 
 export default function RootLayout(){
+
+  const fontsLoaded=useAppFonts()
 
   return(
     <AuthProvider>
       <BudgetProvider>
+        <TransactionProvider>
         <Stack
       screenOptions={{
       headerShown:false
       }}/>
+      </TransactionProvider>
       </BudgetProvider>
     </AuthProvider>
   )
