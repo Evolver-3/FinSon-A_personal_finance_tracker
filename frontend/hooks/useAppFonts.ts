4e8a9167b8bdb2afaa74b3b9df508f9451d3ddef
Ashu Singh <Ashusingh2201@gmail.com>
2026-07-07 21:54:28 +0530

    created a custom hook for fonts, so that it can be used directly

---
+++ frontend/hooks/useAppFonts.ts
@@ -0,0 +1,16 @@
+import {useFonts} from 'expo-font'
+
+export const useAppFonts=()=>{
+  const [fontsLoaded]=useFonts({
+    "Sans-Bold":require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
+    "Sans-Extrabold":require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+    "Sans-Light":require("../assets/fonts/PlusJakartaSans-Light.ttf"),
+    "Sans-Medium":require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
+    "Sans-Regular":require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+    "Sans-Semibold":require("../assets/fonts/PlusJakartaSans-SemiBold.ttf")
+  })
+
+  if(!fontsLoaded) return null;
+  
+  return fontsLoaded
+}
