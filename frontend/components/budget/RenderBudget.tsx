7c63f2572dd914acf55d62111050fc7981e2a925
Ashu Singh <Ashusingh2201@gmail.com>
2026-07-09 23:48:47 +0530

    dark mode styles added and while giving params to the BudgetData page via router.push, now provides budgeId for the individual budget data

--- frontend/components/budget/RenderBudget.tsx
+++ frontend/components/budget/RenderBudget.tsx
@@ -4,18 +4,15 @@ import { getIconByName } from '../comps/Mode/ModalComp'
 import BudgetSpentBar from '../comps/Animate/BudgetSpentBar'
 import { ChevronLeft ,ChevronRight} from 'lucide-react-native'
 import ThemeIcon from '../Theme/ThemeIcon'
-import { router } from 'expo-router'
+import { useRouter } from 'expo-router'
 import PressedAnimate from '../comps/Animate/PressedAnimate'
+import { useTheme } from '@/hooks/useTheme'
+import { formatAmount } from '@/app/(tabs)/home'
 
 
-type renderBudgetProps={
-  item:Budget 
-  spendingBudget:(month:number,year:number)=>Promise<void>
-}
-
-
-const RenderBudget = ({item,spendingBudget}:renderBudgetProps) => {
-  const [checkTransaction,setCheckTransaction]=useState(false)
+const RenderBudget = ({item}:{item:Budget}) => {
+  const {isDark}=useTheme()
+  const router=useRouter()
 
   const spentAmount=Number(item.spent)
   const remainingAmount=Number(item.remaining)
@@ -27,79 +24,72 @@ const RenderBudget = ({item,spendingBudget}:renderBudgetProps) => {
 
   return (
     <View className='px-4'>
-
       <View className="rounded-xl py-6 px-4 mainbg mainborder flex-col gap-y-4"
       style={{
+        backgroundColor:isDark?"#212121":item.category?.color.colors,
         elevation:2
       }}>
 
         <View className='flex-row justify-between  items-center'>
-
-          <View className="flex-row gap-x-5">
+          <View className="flex-row gap-x-5 items-center">
             <View className='rounded-lg p-2'
             style={{
-              backgroundColor:item.category?.color.darkColor
+              backgroundColor:item.category?.color.darkColor,
+              elevation:5
             }}>
-             
-              {getIconByName(item.category?.icon ?? null,false,"#000000",20)}
+              {getIconByName(item.category?.icon ?? null,false,isDark?"#000000":"#ffffff",24)}
             </View>
 
-            <Text className='font-semibold text-xl biggerText'>{item.category?.name}</Text>
+            <Text className=' text-md biggerText uppercase'
+            style={{
+              fontFamily:"Sans-Bold"
+            }}>{item.category?.name}</Text>
           </View>
 
-          {
-            remainingAmount>totalAmount?(
-              <Text className='bg-red-100 text-red-400 px-2 py-1 rounded-xl text-xs font-semibold'>Over Budget</Text>
+          {remainingAmount>totalAmount?(
+              <Text className='bg-red-100 text-red-400 dark:bg-neutral-600 px-2 py-1 rounded-md text-xs font-semibold'>Over Budget</Text>
             ):(
-              <Text className='bg-green-100 text-green-400 px-2 py-1 rounded-xl text-xs font-semibold'>On Track</Text>
-            )
-          }
-
+              <Text className='bg-green-100 dark:bg-neutral-600 text-green-400 px-2 py-1 rounded-md text-xs font-semibold'>On Track</Text>
+            )}
         </View>
 
-        <View className="">
-          <View className="flex-row justify-between">
-            <Text className='text-sm textSmall'>
-              {spentAmount} spent of {totalAmount}
-            </Text>
-            <Text className="text-sm font-semibold textSmall">{perLeft}%</Text>
+        <View className="flex-row justify-between items-center">
+
+          <View className="flex-col gap-y-1">
+              <Text className="text-xs minText"
+              style={{
+                fontFamily:"Sans-Extrabold"
+              }}>Remaining</Text>
+              <Text className="text-green-400 text-md">{formatAmount(remainingAmount)}</Text>
           </View>
 
-          <View className="">
-            <BudgetSpentBar
-            spent={spentAmount}
-            total={totalAmount}
-            />
-          </View> 
-        </View>
-        
-          <View
-          className="h-px mainborder "/>
-
-          <View className="flex-row justify-between items-center">
-
-            <View className="flex-col gap-y-1">
-              <Text className="text-xs font-semibold biggerText">Remaining</Text>
-              <Text className="text-green-400 text-md">{remainingAmount}</Text>
-            </View>
-
-            <PressedAnimate
+          <PressedAnimate
+            style={{
+            width:36,
+            height:36,
+            borderRadius:18,
+            alignItems:"center",
+            justifyContent:"center"
+            }}
+            originalColor={"transparent"}
+            pressedColor={"rgba(99, 102, 241, 0.16)"}
             onPress={()=>{
               router.push({
                 pathname:"/(usertab)/BudgetData",
                 params:{
                   categoryId:item.categoryId,
                   month:String(item.month),
-                  year:String(item.year)
+                  year:String(item.year),
+                  budgetId:item.id
                 }
               })
             }}>
               <ThemeIcon
             icon={ChevronRight}
             size={16}/>
-            </PressedAnimate>
+          </PressedAnimate>
 
-          </View>
+        </View>
         
       </View>
     </View>
