685bed16122fcc90982c58b130cf1b7c50f72dbf
Ashu Singh <Ashusingh2201@gmail.com>
2026-07-11 23:48:37 +0530

    added edit and delete btn to the render content and done some styling for UI

--- frontend/components/account/AccountItem.tsx
+++ frontend/components/account/AccountItem.tsx
@@ -1,9 +1,11 @@
-import { View, Text, Pressable } from 'react-native'
+import { View, Text, Pressable, ActivityIndicator } from 'react-native'
 import React, { useState } from 'react'
 import { getAccountIconByName } from '../comps/Mode/ModalComp'
 import { Pencil } from 'lucide-react-native'
 import UpdateAccount from './UpdateAccount'
 import { useTheme } from '@/hooks/useTheme'
+import { formatAmount } from '@/app/(tabs)/home'
+import PressedAnimate from '../comps/Animate/PressedAnimate'
 
 type AccountItemProps={
   item:Account 
@@ -17,61 +19,127 @@ const AccountItem = ({item,editAccount,removeAccount,loading,error}:AccountItemP
   const [openEdit,setOpenEdit]=useState(false)
   const [selectedAccount,setSelectedAccount]=useState<Account| null>(null)
 
+  
+  const [removePageLoading,setRemovePageLoading]=useState(false)
+
   const {isDark}=useTheme()
+
+  const handledeleteAccount=async()=>{
+        try{
+          setRemovePageLoading(true)
+          if(!item?.id)return
+
+          await removeAccount(item?.id)
+          setOpenEdit(false)
+          
+        }catch(err:any){
+          console.log(error)
+          console.log(err)
+
+        }finally{
+          setRemovePageLoading(false)
+        }
+      }
   
   return (
 
     <View className='px-4'>
       <View
-      className='flex-row items-center mainborder  rounded-xl p-4 '
+      className='rounded-xl py-6 px-4 mainbg mainborder flex-col gap-y-4'
       style={{
-        backgroundColor:isDark? "#212121":item.color.colors,
+        backgroundColor:isDark?"#212121":item.color.colors,
         elevation:2
-        }}>
-        <View className='flex-grow flex-row gap-x-4 items-center'>
+      }}>
+        <View className='flex-grow flex-row gap-x-5 items-center'>
           <View
           className='p-2 items-center rounded-lg '
           style={{
             elevation:5,
             backgroundColor:item.color.darkColor
           }}>
-            {getAccountIconByName(item.icon,false,"#000000")}
-          </View>
-       
-          <View className=''>
-            <View className='flex-row items-center gap-x-4'>
-              <Text className='text-lg text-neutral-500'>
-                {item.name}
-              </Text>
-              <View className='p-[2px] rounded-xl'
-              style={{
-                backgroundColor:`${item.color.colors}`
-              }}>
-                <Text
-                style={{
-                color:`${item.color.btncolor}`,
-                fontSize:8
-                }}>
-                  {item.type}
-                </Text>
-              </View>
-            </View>
-            <Text 
-              className={`text-sm text-neutral-700 font-semibold `}>
-                  {item.balance}
-              </Text>
+            {getAccountIconByName(item.icon,false,isDark?"#000000":"#ffffff",24)}
           </View>
 
+          <View>
+          <Text 
+            className=' text-md biggerText uppercase'
+            style={{
+              fontFamily:"Sans-Bold"
+            }}>
+              {item.name}
+          </Text>
+
+          <Text 
+            className="text-green-400 text-md">
+              {formatAmount(item.balance)}
+          </Text>
+          </View>
+
+          <View 
+            className='px-2 py-1 rounded-md'
+                  style={{
+                      backgroundColor:"#C4B6D2"
+                    }}>
+                <Text
+                style={{
+                          color:isDark?"#000000":"#ffffff",
+                          fontSize:8,
+                          fontFamily:"Sans-Semibold"
+                        }}>
+                  {item.type}
+                </Text>
+            
+          </View>
+
+        <View className='flex-row gap-x-5 justify-end flex-grow'>
+          <PressedAnimate
+            onPress={()=>setOpenEdit(true)}
+            originalColor={isDark?"#518151":"#ADF7B3"}
+            pressedColor={isDark?"#49B649":"#CEE9D0"}
+            style={{
+              width:40,
+              height:20,
+              borderRadius:5,
+              alignItems:"center",
+              justifyContent:"center",
+              elevation:6}}>
+                    
+            <Text
+              className="smallText text-xs"
+              style={{
+                      fontFamily:"Sans-Semibold"
+                    }}>
+                      Edit
+            </Text>
+          </PressedAnimate>
+                    
+          <PressedAnimate
+            onPress={handledeleteAccount}
+            originalColor={isDark?"#753B2F":"#D28E89"}
+            pressedColor={"#EAA59E"}
+            style={{
+              width:60,
+              height:20,
+              borderRadius:5,
+              alignItems:'center',
+              justifyContent:"center",
+              elevation:6}}>
+        
+              {removePageLoading ?
+              (<ActivityIndicator color={"#ffffff"} size={10}/>):
+              <Text 
+                className="smallText text-xs"
+                style={{
+                          fontFamily:"Sans-Semibold"
+                      }}>
+                  Remove
+              </Text>}
+          </PressedAnimate>
         </View>
-       
-        <Pressable
-          onPress={()=>{
-          setOpenEdit(true)
-          setSelectedAccount(item)}}>
-            <Pencil
-              color={'#000000'}
-              size={18}/>
-        </Pressable>
+
+        </View>
+
+      
     
       </View>
    
@@ -80,7 +148,6 @@ const AccountItem = ({item,editAccount,removeAccount,loading,error}:AccountItemP
       setOpenEdit={setOpenEdit}
       account={selectedAccount}
       editAccount={editAccount}
-      removeAccount={removeAccount}
       loading={loading}
       error={error}/>
                  
