71f2fe30585e126ef0fdc64e80445905072af1a3
Ashu Singh <Ashusingh2201@gmail.com>
2026-07-11 23:45:47 +0530

    give the add icon its own layout style for better UI, also redeclared some styles for all other icons

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -2,10 +2,13 @@ import { useAuthContext } from "@/context/AuthContext"
 import { Redirect, Tabs } from "expo-router"
 import { ActivityIndicator, View ,Text} from "react-native"
 import { bottomTabData } from "@/data"
+import { Plus } from "lucide-react-native"
+import { useTheme } from "@/hooks/useTheme"
 
 
 export default function TabsLayout() {
   const { user,loading } = useAuthContext()
+  const {isDark}=useTheme()
 
   if (loading) {
     return (
@@ -29,37 +32,63 @@ export default function TabsLayout() {
               bottom:0,
               height:60,
               borderRadius:0,
-              borderTopStartRadius:10,
-              borderTopEndRadius:10,
-              elevation:0,
+              elevation:5,
               borderTopWidth:0,
-              backgroundColor:"#383534",
+              backgroundColor:isDark?"#383534":"#EDEDED",
               gap:0
             },
             tabBarItemStyle:{
-              paddingVertical:10,
+              paddingVertical:5,
               flex:1
 
             }
           }}>
-            {bottomTabData.map((data)=>(
-            <Tabs.Screen
-            key={data.id}
-            name={data.name}
+            
+            {bottomTabData.map((data)=>{
+
+              if(data.name==="Add"){
+                return <Tabs.Screen 
+                name="Add"
+                options={{
+                  tabBarIcon:({focused})=>(
+                    <View style={{
+                      width:60,
+                      height:60,
+                      borderRadius:28,
+                      backgroundColor:focused?"#3DB0C2":"#36C9D1",
+                      alignItems:"center",
+                      justifyContent:"center",
+                      marginBottom:30,
+                      elevation:5,
+                      shadowColor:"#36C9D1",
+                      shadowOffset:{width:0,height:4},
+                      shadowOpacity:0.4,
+                      shadowRadius:8,
+                      
+                    }}>
+                      <Plus color="#fff" size={28}/>
+                    </View>
+                  )
+                }}/>
+              }
+
+              return(
+              <Tabs.Screen
+              key={data.id}
+              name={data.name}
          
-            options={{
-              tabBarIcon:({focused})=>(
-                <TabIcon
-                text={data.text}
-                focused={focused}
-                icon={data.icon(focused)} />
+              options={{
+                tabBarIcon:({focused})=>(
+                  <TabIcon
+                  text={data.text}
+                  focused={focused}
+                  icon={data.icon(focused ,24,isDark? (focused? "#AAAAAA":"#ffffff"):"#000000")} />
+                )
+              }}/>
               )
-            }}/>
-           ))}
+            })}
             </Tabs> 
 }
-
-
 
 type TabIconProps={
   focused:boolean 
@@ -68,19 +97,17 @@ type TabIconProps={
 }
 
 const TabIcon=({focused,icon,text}:TabIconProps)=>{
+  const {isDark}=useTheme()
   return (
-    <View className="flex-1 items-center justify-center"
+    <View className="flex-1 flex-col items-center mb-4"
     >
-      <View>
         {icon}
-      </View>
-        <View>
-          <Text className=' text-font-extralight leading-tight text-center text-[10px]'
+        
+         <Text className=' text-font-extralight leading-tight text-center text-[10px] w-full'
         style={{
-         color:focused ? "#0a0a0a" : "#a3a3a3"
+         color:focused ? (isDark?"#AAAAAA":"#0a0a0a") :( isDark?"#ffffff":"#000000")
         }}>{text}</Text>
-        </View>
-      
+
     </View>
   )
 }
