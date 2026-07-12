a9d392b571497efc0a6803549850521952fd5363
Ashu Singh <Ashusingh2201@gmail.com>
2026-07-12 23:34:56 +0530

    In this comp, whole required form data is here for to use as props

---
+++ frontend/components/transactionPage/TransactionForm.tsx
@@ -0,0 +1,215 @@
+import { View, Pressable, Platform, TextInput } from 'react-native'
+import React,{useState,useEffect} from 'react'
+import { TextData } from '../comps/TextData'
+import ModalComp, { SelectType } from '../comps/Mode/ModalComp'
+import { CalendarRangeIcon } from 'lucide-react-native'
+import SelectDropdown from '../comps/SelectDropdown'
+import { ButtonNeed } from '../comps/ButtonNeed'
+import DateTimePicker from '@react-native-community/datetimepicker'
+import ThemeIcon from '../Theme/ThemeIcon'
+import ErrorPopUp from '../comps/ErrorPopUp'
+import Wrapper from '../WrapperPage'
+import { useTheme } from '@/hooks/useTheme'
+
+type initialValuesTransactionProps={
+  id?:string
+  title?:string 
+  amount?:string 
+  type?:"INCOME"|"EXPENSE" 
+  note?:string 
+  date?:Date
+  accountId?:string 
+  categoryId?:string
+}
+
+type TransactionFormProps={
+  initialValues?:Partial<initialValuesTransactionProps>
+    accounts:Account[]
+    categories:Category[]
+    onSubmit:(values:createTransactionProps)=>Promise<void>
+    submitText:string 
+    loading:boolean
+    error:string | null
+}
+
+const TransactionForm = ({ initialValues,
+  accounts,
+  categories,
+  onSubmit,
+  submitText,
+  loading,
+  error}:TransactionFormProps) => {
+
+   const {isDark}=useTheme()
+  
+    const [title,setTitle]=useState(initialValues?.title ?? "")
+      const [amount,setAmount]=useState(initialValues?.amount ??"")
+      const [type,setType]=useState<"INCOME"|"EXPENSE">(initialValues?.type ??"EXPENSE")
+      const [note,setNote]=useState(initialValues?.note ?? "")
+      const [date,setDate]=useState<Date>( initialValues?.date ?? new Date())
+      const [accountId,setAccountId]=useState(initialValues?.accountId ??'')
+      const [categoryId,setCategoryId]=useState(initialValues?.categoryId ??'')
+  
+      
+      const [datePickerOpen,setDatePickerOpen]=useState(false)
+    
+      useEffect(()=>{
+        if(initialValues){
+          setTitle(initialValues?.title ?? "")
+          setAmount(initialValues?.amount ?? "")
+          setType(initialValues?.type ??"EXPENSE")
+          setNote(initialValues?.note ?? "")
+          setDate(initialValues?.date ?? new Date())
+          setAccountId(initialValues?.accountId ?? "")
+          setCategoryId(initialValues?.categoryId ?? "")
+        }
+        
+      },[initialValues?.id])
+  
+      const handleSubmit=async()=>{
+   
+        onSubmit({title,amount,type,note,accountId,categoryId,date})
+          setTitle("")
+          setAmount("")
+          setType("EXPENSE")
+          setNote("")
+          setDate(new Date())
+          setAccountId("")
+          setCategoryId("")
+  
+      }
+      
+       const formattedDate=date? date.toLocaleDateString('en-IN',{
+        day:"numeric",
+        month:"short",
+        year:"numeric"
+       }):'Select Date'
+  return (
+    <View className="relative flex-col gap-y-5">
+    
+    
+          <ErrorPopUp
+          errorMessage={error}/>
+    
+          
+           <TextData
+            keyboardType="default"
+            tagexist={false}
+            tag={''}
+            value={title}
+            placeholder='Enter Title'
+            onChangeText={setTitle}
+            secureTextEntry={false}/>
+    
+             <TextData
+            keyboardType="number-pad"
+            tagexist={false}
+            tag={''}
+            value={amount}
+            placeholder='Enter amount'
+            onChangeText={setAmount}
+            secureTextEntry={false}/>
+    
+             <TextData
+            keyboardType="default"
+            tagexist={false}
+            tag={''}
+            value={note}
+            placeholder='Enter note'
+            onChangeText={setNote}
+            secureTextEntry={false}
+            multiline={true}
+            textAlignVertical="top"
+            lines={4}
+            />
+    
+            <View className='items-center justify-center mainborder rounded-lg'>
+              <View className="flex-row gap-x-3 p-1 py-1 mainbg rounded-lg"
+              style={{
+                elevation:2
+              }}>
+              <SelectType
+              focused={type==="INCOME"}
+              onPress={()=>setType("INCOME")}
+              text='Income'/>
+               
+              <SelectType
+              focused={type==="EXPENSE"}
+              onPress={()=>setType("EXPENSE")}
+              text='Expense'/>
+              </View>
+            </View>
+    
+            <View className="flex-row items-center justify-center gap-x-4">
+    
+              <TextInput
+              value={formattedDate}
+              editable={false}
+              className="mainbg"
+              style={{
+                paddingHorizontal:20,
+                paddingVertical:6,
+                borderRadius:10,
+                textAlign:"center",
+                elevation:2,
+                color:isDark?"#ffffff":"#000000"
+              }}
+              />
+              <Pressable
+              onPress={()=>setDatePickerOpen(true)}>
+                <ThemeIcon
+                icon={CalendarRangeIcon}
+                size={20}/>
+              </Pressable>
+    
+              {datePickerOpen &&(
+                <DateTimePicker
+                value={date ?? new Date()}
+                mode="date"
+                display={Platform.OS==="ios"?"spinner":"calendar"}
+                onChange={(event,pickedDate)=>{
+                  if(Platform.OS!=="ios"){
+                    setDatePickerOpen(false)
+                  }
+                  if(pickedDate){
+                    setDate(pickedDate)
+                  }
+                }}
+                />
+              )}
+            </View>
+           
+            <View className='flex-row items-center justify-center gap-x-3'>
+             <SelectDropdown
+             options={accounts.map(acc=>({
+              id:acc.id,
+              name:acc.name,
+              color:acc.color
+             }))}
+             selectedId={accountId}
+             onSelect={setAccountId}
+             placeholder='Select Account'/>
+    
+             <SelectDropdown
+             options={categories.map(cat=>({
+              id:cat.id,
+              name:cat.name,
+              color:cat.color
+             }))}
+             selectedId={categoryId}
+             onSelect={setCategoryId}
+             placeholder='Select Category'/>
+            </View>
+    
+            <ButtonNeed
+            onPress={handleSubmit}
+            text={submitText}
+            disabled={loading}
+            loading={loading}
+            style={{}}
+            />
+              </View>
+  )
+}
+
+export default TransactionForm
