454ef243f8de4208e967369af2f3d3be32066298
Ashu Singh <Ashusingh2201@gmail.com>
2026-07-13 22:21:19 +0530

    provided currenyContext here

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -5,6 +5,7 @@ import { AuthProvider } from "../context/AuthContext";
 import { BudgetProvider } from "@/context/BudgetContext"
 import { TransactionProvider } from "@/context/TransactionContext";
 import { useAppFonts } from "@/hooks/useAppFonts";
+import { CurrencyProvider } from "@/context/CurrencyContext";
 
 export default function RootLayout(){
 
@@ -16,10 +17,12 @@ export default function RootLayout(){
     <AuthProvider>
       <BudgetProvider>
         <TransactionProvider>
+          <CurrencyProvider>
         <Stack
       screenOptions={{
       headerShown:false
       }}/>
+      </CurrencyProvider>
       </TransactionProvider>
       </BudgetProvider>
     </AuthProvider>
