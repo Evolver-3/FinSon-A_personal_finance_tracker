e11c8945ffa5d9fd0ff007a24b2963aadbe842ec
Ashu Singh <Ashusingh2201@gmail.com>
2026-07-23 00:05:13 +0530

    added a state to check if the user token is initialized or not

--- frontend/context/AuthContext.tsx
+++ frontend/context/AuthContext.tsx
@@ -1,4 +1,3 @@
-import { View, Text } from 'react-native'
 import React, { createContext,useContext,useEffect,useState } from 'react'
 import { clearTokens, getAccessToken } from '@/services/tokenStorage'
 import { getProfile } from '@/services/userServices'
@@ -9,20 +8,39 @@ export const AuthProvider=({children}:{children:React.ReactNode})=>{
 
   const [user,setUser]=useState<User|null>(null)
   const [loading,setLoading]=useState(true)
+  const [initialized,setInitialized]=useState(false)
 
 
     const loadUser=async()=>{
+
+      if(initialized)return;
+
       try{ 
+
+        console.log("loadUser: starting...")
+        setLoading(true)
         const token=await getAccessToken()
 
+        console.log('loadUser: token?',token?'exists':"missing")
+
         if(!token){
+
+          console.log('loadUser: no token, setting user null')
           setUser(null)
           return
         }
 
+        console.log('loadUser: calling getProfile...')
+
         const res=await getProfile()
+
+        console.log("loadUser: getProfile returned:", JSON.stringify(res, null, 2));
     
-        setUser(res.data)
+        console.log("loadUser: res.data?", res.data);
+
+
+        setUser(res.data || res)
+
 
       }catch(error:any){
        if(error?.response?.status=== 401){
@@ -31,14 +49,15 @@ export const AuthProvider=({children}:{children:React.ReactNode})=>{
        }
       }finally{
         setLoading(false)
+        setInitialized(true)
       }
     }
   useEffect(()=>{
-    loadUser()
+      loadUser()
   },[])
 
   return(
-    <AuthContext.Provider value={{user,loading,setUser,loadUser}}>
+    <AuthContext.Provider value={{user,loading,setUser,loadUser,initialized}}>
       {children}
     </AuthContext.Provider>
   )
