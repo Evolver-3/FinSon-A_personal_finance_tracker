3fa423baf20c9a84f3d278c674b2fe6571eee178
Ashu Singh <Ashusingh2201@gmail.com>
2026-07-23 00:06:27 +0530

    removed unused services and added for loginwithcode and with token google services

--- frontend/services/authServices.ts
+++ frontend/services/authServices.ts
@@ -1,25 +1,11 @@
 import {api} from './api'
 
-export const registerUser=async(data:registrationProps)=>{
-  const res=await api.post("/auth/register",data)
-
-  return res.data
+export const googleLoginWithCode=(data:createLoginPageHookProps)=>{
+  return api.post("/auth/google/code",data)
 }
 
-export const loginUser=async(data:loginProps)=>{
-  const res=await api.post("/auth/login",data)
-  
-  return res.data
-}
-
-export const verifyEmail=async(token:string)=>{
-  const res=await api.get(`/auth/verify-email?token=${token}`)
-  return res.data
-}
-
-export const resendEmail=async(email:string)=>{
-  const res=await api.post("/auth/resend-email",{email})
-  return res.data
+export const googleLogin=(idToken:string)=>{
+  return api.post("/auth/google",{idToken})
 }
 
 export const refreshToken=async(refreshToken:string)=>{
@@ -36,24 +22,6 @@ export const logoutAllDevice=async()=>{
   const res=await api.post("/auth/logoutAllDevice")
   return res.data
 }
-
-export const passwordReset=async(data:changePasswordProps)=>{
-  const res=await api.post("/auth/password-reset",data)
-  return res.data
-}
-
-
-export const forgotPassword=async(email:string)=>{
-  const res=await api.post("/auth/forgot-password",{email})
-  return res.data
-}
-
-
-export const resetPassword=async(token:string,data:resetPasswordProps)=>{
-  const res=await api.post(`/auth/reset-password?token=${token}`,data)
-  return res.data
-}
-
 
 
 
