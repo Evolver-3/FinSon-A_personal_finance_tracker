3b0bc10d15468d9f4b790a63d5536ee0ffcd3c36
Ashu Singh <Ashusingh2201@gmail.com>
2026-07-27 23:18:19 +0530

    refactor(auth.routes): remove Google authentication routes and add sync endpoint

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,20 +1,15 @@
 import { Router } from "express";
-import {  logoutUser, AllLogout, refreshAccessToken,googleLogin,googleLoginWithCode} from "../controllers/auth.controllers.js";
+import {  logoutUser, AllLogout,userController,syncUserController} from "../controllers/auth.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { verifyClerk } from "../middlewares/verify.middleware.js";
 
 const authRouter=Router()
 
-//auth with google
 
-authRouter.route("/google").post(googleLogin)
-authRouter.route("/google/code").post(googleLoginWithCode);
+authRouter.route("/sync").post(verifyClerk,syncUserController)
 
+authRouter.route("/user").get(verifyJWT,userController)
 
-
-
-authRouter.route("/refreshToken").post(refreshAccessToken)
-
-//logout 
 authRouter.route("/logout").post(logoutUser)
 
 //logout-all devices
