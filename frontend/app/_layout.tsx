f9509efbfd17e6459228dd651ee909bf50ecf5a9
Ashu Singh <Ashusingh2201@gmail.com>
2026-07-28 17:49:48 +0530

    feat(RootLayout): integrate ClerkProvider and AuthTokenSync for authentication management

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -6,15 +6,24 @@ import { BudgetProvider } from "@/context/BudgetContext"
 import { TransactionProvider } from "@/context/TransactionContext";
 import { useAppFonts } from "@/hooks/useAppFonts";
 import { CurrencyProvider } from "@/context/CurrencyContext";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenStorage } from "@/services/tokenStorage";
+import { AuthTokenSync } from "@/components/AuthTokenSync";
 
 export default function RootLayout(){
 
   const fontsLoaded=useAppFonts()
 
   if(!fontsLoaded) return null;
-
+  
   return(
-    <AuthProvider>
+    <ClerkProvider 
+    publishableKey={
+      process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+    }
+    tokenCache={tokenStorage}>
+    <AuthTokenSync/>
+      <AuthProvider>
       <BudgetProvider>
         <TransactionProvider>
           <CurrencyProvider>
@@ -26,5 +35,6 @@ export default function RootLayout(){
       </TransactionProvider>
       </BudgetProvider>
     </AuthProvider>
+    </ClerkProvider>
   )
 }
