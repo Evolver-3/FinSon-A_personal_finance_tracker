4e4c5d958aafdfcb5383ff2a5ceda325fbf0b5a0
Ashu Singh <Ashusingh2201@gmail.com>
2026-08-01 23:53:00 +0530

    feat(aiRoutes): add GET endpoint for retrieving insights by period

--- backend/src/routes/ai.routes.ts
+++ backend/src/routes/ai.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import {financeInsightsControllers} from '../controllers/ai.controllers.js'
+import {financeInsightsControllers,getInsights} from '../controllers/ai.controllers.js'
 
 const aiRouter=Router()
 
 aiRouter.use(verifyJWT)
 
 aiRouter.route("/insights").post(financeInsightsControllers)
+
+aiRouter.route('/insights/period').get(getInsights)
 
 export default aiRouter
