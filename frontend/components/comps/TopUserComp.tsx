412eab4a278d8ff7f22d68b2c83f51e8efa148a0
Ashu Singh <Ashusingh2201@gmail.com>
2026-08-02 22:25:21 +0530

    refactor(TopUserComp): improve layout structure and enhance avatar handling

--- frontend/components/comps/TopUserComp.tsx
+++ frontend/components/comps/TopUserComp.tsx
@@ -1,11 +1,9 @@
 import { View, Text ,Image} from 'react-native'
 import React,{useEffect} from 'react'
 import { useUser } from '@/hooks/useUser'
-import { UserRound } from 'lucide-react-native'
+import { BellIcon, UserRound } from 'lucide-react-native'
 
-type headingTextProps={
-  headingText:string 
-}
+
 const TopUserComp = ({headingText}:headingTextProps) => {
    const {fetchUserProfile,user,loading}=useUser()
 
@@ -13,19 +11,31 @@ const TopUserComp = ({headingText}:headingTextProps) => {
       
     fetchUserProfile()
    },[])
-  return (
-    <View className="flex-row items-center gap-x-6 ">
 
-      {user?.avatar ? (
+  return (
+    <View className="topHead">
+
+      <View className="imageText">
+        {user?.avatar ? (
         <Image 
         source={{uri:user?.avatar as any}}
-        style={{width:30,height:30, borderRadius:40}}/>):(
+        className='mainImage borderOne'
+        />):(
           <UserRound
           size={30}
           color={"#ffffff"}/>
         )}  
            
-      <Text className='text-neutral-700 dark:text-white text-lg '>{ headingText}</Text>  
+      <Text className='biggerText topHeadings'
+      style={{
+        fontFamily:"Sans-Extrabold"
+      }}>{ headingText}</Text>  
+      </View>
+
+      <View className="bellIcon  hoverBorder">
+        <BellIcon size={20} color={"#ffffff"}/>
+      </View>
+
               
     </View>
   )
