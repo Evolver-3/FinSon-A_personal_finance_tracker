0b84b41493be7a68444a1dbacddd39d70fe8343a
Ashu Singh <Ashusingh2201@gmail.com>
2026-08-03 23:56:07 +0530

    feat(transaction.services): add getTransactionByMonth function to retrieve transactions by month and year

--- frontend/services/transactionServices.ts
+++ frontend/services/transactionServices.ts
@@ -15,6 +15,15 @@ export const getTransaction=async(transactionId:string)=>{
   return res.data
 }
 
+export const getTransactionByMonth=async(year:number, month:number)=>{
+  // console.log("does it reaches service:")
+  const res=await api.get(`/transaction/period?year=${year}&month=${month}`)
+
+  // console.log("data from service:", res.data)
+
+  return res.data
+}
+
 
 export const updateTransaction=async(transactionId:string,data:updateTransactionProps)=>{
   const res=await api.patch(`/transaction/${transactionId}`,data)
