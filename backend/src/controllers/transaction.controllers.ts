5b9e4ceb4c078a316a8ebad1f99467579a63efc1
Ashu Singh <Ashusingh2201@gmail.com>
2026-08-05 23:36:28 +0530

    refactor(transaction.controllers): simplify response structure in getTransactionByYear

--- backend/src/controllers/transaction.controllers.ts
+++ backend/src/controllers/transaction.controllers.ts
@@ -223,8 +223,7 @@ export const getTransactionByYear=asyncHandler(async(req,res)=>{
   return res.status(200).json(
     new ApiResponse(200,{
       year,
-      months:result,
-      transactionsdetails
+      months:result
     },"Transactions fetched")
   )
 })
