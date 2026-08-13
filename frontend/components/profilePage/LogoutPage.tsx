73d89a78de8ea94244ecee193530124b692209ce
Ashu Singh <Ashusingh2201@gmail.com>
2026-08-13 22:52:56 +0530

    refactor(LogoutPage): streamline logout process and enhance loading state handling

--- frontend/components/profilePage/LogoutPage.tsx
+++ frontend/components/profilePage/LogoutPage.tsx
@@ -1,44 +1,47 @@
-import { View, Text, Pressable } from 'react-native'
-import React from 'react'
-import { useAuth } from '@/hooks/useAuth'
-import { LogOut } from 'lucide-react-native'
-import { clearTokens, getAccessToken } from '@/services/tokenStorage'
+import { View, Text, Pressable, ActivityIndicator } from 'react-native'
+import React,{useState} from 'react'
 import { router } from 'expo-router'
 import { useAuthContext } from '@/context/AuthContext'
-import ThemeIcon from '../Theme/ThemeIcon'
+import { useClerk } from '@clerk/clerk-expo'
+import { useTheme } from '@/hooks/useTheme'
 
 const LogoutPage = () => {
 
-  const { logoutUser}=useAuth()
+  const [loading,setLoading]=useState(false)
+  const { signOut}=useClerk()
   const {setUser}=useAuthContext()
+  const {isDark}=useTheme()
 
   const handleLogout=async()=>{
-    try{
-      const token=await getAccessToken()
 
-      if(token){
-        await logoutUser(token)
-      }
-      setUser(null)
-      router.replace("/(auth)/sign-in")
-    }catch(error){
-      await clearTokens()
-      setUser(null)
-      router.replace("/(auth)/sign-in")
+    setLoading(true)
+    try{
+      await signOut()
+   setUser(null)
+   router.replace("/(auth)/Login")
+    }finally{
+      setLoading(false)
     }
+ 
 
   }
   return (
-    <View className='mt-10 border dark:border-red-400 border-red-200  rounded-md py-3 items-center bg-red-300 dark:bg-red-500'>
+    <View className='mt-10  rounded-md items-center bg-white dark:bg-[#1B1C1B]'>
+
       <Pressable
-      onPress={handleLogout}>
-        <View className='flex-row items-center gap-x-2'>
-          <ThemeIcon
-          icon={LogOut}
-          size={20}/>
+      onPress={handleLogout}
+      android_ripple={{color:isDark?'#1F1F21':"",foreground:true}}
+      className="w-full items-center py-5">
+        {loading ? (
+          <View className='flex-row items-center gap-x-5 '>
+          <Text className='text-black dark:text-white font-semibold text-lg'>Log Out</Text>
+          <ActivityIndicator color={isDark?"#ffffff":"#000000"}/>
+        </View>
+        ):(
+          <View className='flex-row items-center gap-x-2'>
           <Text className='text-black dark:text-white font-semibold text-lg'>Log Out</Text>
         </View>
-
+        )}
 
       </Pressable>
     </View>
