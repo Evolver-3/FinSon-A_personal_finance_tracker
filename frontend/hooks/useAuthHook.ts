9b15683d0fb870fee056bc564f4331407185fb15
Ashu Singh <Ashusingh2201@gmail.com>
2026-08-18 19:23:41 +0530

    to check if the useAuthContext layer is existing or not, if does then can use various function from it

--- frontend/hooks/useAuthHook.ts
+++ frontend/hooks/useAuthHook.ts
@@ -1,50 +1,12 @@
-import { refreshToken, logout, logoutAllDevice, syncbackend} from "@/services/authServices";
-import { useState } from "react";
-
+import { AuthContext } from "@/context/AuthContext"
+import { useContext } from "react"
 
 export const useAuthHook=()=>{
-  const [error,setError]=useState<string|null>(null)
-  const [loading,setLoading]=useState(false)
+  const context=useContext(AuthContext)
 
-
-
-  const run=async<T>(fn:()=>Promise<T>)=>{
-
-    try{
-
-      setLoading(true)
-      setError(null)
-
-      const data=await fn()
-      return data
-
-    }catch(err:any){
-      const message=err?.response?.data?.message || err?.message || "Something went wrong"
-      console.log("full error:", JSON.stringify(err?.response?.data))
-
-      setError(message)
-      throw err
-
-    }finally{
-      setLoading(false)
-    }
+  if(!context){
+    throw new Error("useAuthContext must be used inside AuthProvider")
   }
 
-  return {
-    loading,
-    error,
-
-    syncBackendHook:(token:string)=>run(async()=>{
-      const res=await syncbackend(token)
-      console.log("token in hook:",token)
-      return res
-    }),
-
-    refreshAuthToken: (token: string) =>
-      run(() => refreshToken(token)),
-
-
-    logoutEverywhere: () =>
-      run(() => logoutAllDevice())
-  }
+  return context
 }
