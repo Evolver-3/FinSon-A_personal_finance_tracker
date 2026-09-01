6690333b7c72f42e2fb698008c0df231b5e6bf6f
Ashu Singh <Ashusingh2201@gmail.com>
2026-09-01 21:56:59 +0530

    Refactor DateFormat component to improve date and amount formatting functions

--- frontend/components/comps/DateFormat.tsx
+++ frontend/components/comps/DateFormat.tsx
@@ -7,3 +7,15 @@ export const formatDate=(date?:string)=>{
     year:"numeric"
   })
 }
+
+export const formatAmount=(amount:Number | string)=>{
+  return Number(amount).toLocaleString("en-IN");
+}
+
+export const formatMonth=(month?:number)=>{
+  if(!month) return '';
+
+  return new Date(month).toLocaleDateString("en-GB",{
+    month:"long"
+  })
+}
